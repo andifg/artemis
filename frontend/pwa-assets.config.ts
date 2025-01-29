cd8f29ad5bfd024578b4df10d998e1e25284200d
anixel <45008199+andifg@users.noreply.github.com>
2025-01-29 08:41:29 +0100

    format frontend

--- frontend/pwa-assets.config.ts
+++ frontend/pwa-assets.config.ts
@@ -1,14 +1,14 @@
 import {
-    defineConfig,
-    minimal2023Preset,
-  } from '@vite-pwa/assets-generator/config'
+  defineConfig,
+  minimal2023Preset,
+} from "@vite-pwa/assets-generator/config";
 
-  export default defineConfig({
-    preset: {
-        ...minimal2023Preset,
-        apple: {
-            sizes: [180],
-            padding: 0
-        }
-    }
-  })
+export default defineConfig({
+  preset: {
+    ...minimal2023Preset,
+    apple: {
+      sizes: [180],
+      padding: 0,
+    },
+  },
+});
