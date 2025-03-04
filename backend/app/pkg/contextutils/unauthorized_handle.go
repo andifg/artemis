97cb6959f9938281fe534fa89bdd50854c2dd131
anixel <45008199+andifg@users.noreply.github.com>
2025-03-04 10:11:16 +0100

    fix token removal during unauthenticated handler

--- backend/app/pkg/contextutils/unauthorized_handle.go
+++ backend/app/pkg/contextutils/unauthorized_handle.go
@@ -7,9 +7,9 @@ import (
 
 func UnauthorizedHandler(c *gin.Context) {
 	log.Debug(("Delete tokens and return 401"))
-	c.SetCookie("access_token", "", -1, "/", "localhost", false, true)
-	c.SetCookie("refresh_token", "", -1, "/", "localhost", false, true)
-	c.SetCookie("id_token", "", -1, "/", "localhost", false, false)
+	c.SetCookie("access_token", "", -1, "/", c.Request.Host, false, true)
+	c.SetCookie("refresh_token", "", -1, "/", c.Request.Host, false, true)
+	c.SetCookie("id_token", "", -1, "/", c.Request.Host, false, false)
 	c.JSON(401, gin.H{"error": "Unauthorized"})
 	c.Abort()
 }
