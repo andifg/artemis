ad66396b1e2d3a63554148d3697601dce0840fe5
anixel <45008199+andifg@users.noreply.github.com>
2025-03-27 19:24:40 +0100

    add yaxis and labelist for chart

--- frontend/src/components/meatPortionsChart/MeatPortionBarChartContainer.tsx
+++ frontend/src/components/meatPortionsChart/MeatPortionBarChartContainer.tsx
@@ -1,16 +1,21 @@
 import "./meatPortionsChart.scss";
-import {
-  ChartConfig,
-  ChartContainer,
-  ChartTooltip,
-  ChartTooltipContent,
-} from "../ui/chart";
+import { ChartConfig, ChartContainer } from "../ui/chart";
 import { CategoricalChartProps } from "recharts/types/chart/generateCategoricalChart";
-import { Bar, BarChart, XAxis } from "recharts";
+import {
+  Bar,
+  BarChart,
+  XAxis,
+  LabelList,
+  YAxis,
+  CartesianGrid,
+} from "recharts";
 import { DataKey } from "recharts/types/util/types";
 import { AggregatedMeatPortions } from "@/client/types";
 import { Skeleton } from "../ui/skeleton";
+import { ReactNode, useEffect } from "react";
 
+import { LabelProps } from "recharts";
+import { SVGProps } from "react";
 const chartConfig = {
   desktop: {
     label: "Meat Portions",
@@ -31,11 +36,50 @@ const MeatPortionBarChartContainer = <T,>({
   loading,
   aggregatedMeatPortions,
 }: MeatPortionBarChartContainerProps<T>) => {
+  useEffect(() => {
+    console.log("Meat portion chart data: ", data);
+  }, [data]);
+
+  const renderCustomizedLabel = (
+    props: Omit<SVGProps<SVGTextElement>, "viewBox"> & LabelProps,
+  ): ReactNode => {
+    const { x, y, width, value } = props;
+
+    if (data == undefined || value == undefined) {
+      return y;
+    }
+
+    return (
+      <>
+        {Number(value) > 0 && (
+          <g>
+            <text
+              x={Number(x) + Number(width) / 2}
+              y={Number(y) - 8}
+              fill="#3E721D"
+              textAnchor="middle"
+              dominantBaseline="middle"
+            >
+              {value}
+            </text>
+          </g>
+        )}
+      </>
+    );
+  };
+
   return loading && aggregatedMeatPortions == undefined ? (
     <Skeleton className="min-h-[200px] w-full m-2 self-center" />
   ) : (
     <ChartContainer config={chartConfig} className="min-h-[200px] w-full">
-      <BarChart accessibilityLayer data={data}>
+      <BarChart
+        accessibilityLayer
+        data={data?.sort((a, b) =>
+          a.TimeframeStart.localeCompare(b.TimeframeStart),
+        )}
+        margin={{ top: 30, left: 0 }}
+      >
+        <CartesianGrid vertical={false} />
         <XAxis
           dataKey={dataKey}
           tickLine={false}
@@ -43,13 +87,20 @@ const MeatPortionBarChartContainer = <T,>({
           axisLine={true}
           tickFormatter={(value) => value.slice(0, 3)}
         />
-        <ChartTooltip content={<ChartTooltipContent />} />
+        <YAxis width={25} allowDecimals={false} />
         <Bar
           dataKey="Total"
           name="Total Portions"
           fill="var(--meat-color)"
           radius={4}
-        />
+        >
+          <LabelList
+            dataKey="Total"
+            position="insideTop"
+            // style={{ fill: "white" }}
+            content={renderCustomizedLabel}
+          />
+        </Bar>
       </BarChart>
     </ChartContainer>
   );
