bb86d4a311bad381bbd01df9642c747fa460d6dc
anixel <45008199+andifg@users.noreply.github.com>
2025-04-05 14:29:56 +0200

    switch add icon to generic one

--- frontend/src/components/BottomNavigator/BottomNavigator.tsx
+++ frontend/src/components/BottomNavigator/BottomNavigator.tsx
@@ -1,5 +1,4 @@
 import "./bottomNavigator.scss";
-import TestSvg from "../../assets/meat.svg";
 import { MealModal } from "@/Components/MealModal/MealModal";
 import { useState } from "react";
 import { AlignJustify } from "lucide-react";
@@ -8,6 +7,7 @@ import { useLocation, Link } from "react-router";
 import { useCentralState } from "@/hooks/useCentralState";
 import { CircleUserRound } from "lucide-react";
 import { Trophy } from "lucide-react";
+import { CirclePlus } from "lucide-react";
 
 function BottomNavigator() {
   const [openAddModal, setOpenAddModal] = useState<boolean>(false);
@@ -28,49 +28,65 @@ function BottomNavigator() {
   return (
     <div className="bottom-navigator-wrapper">
       <div className="bottom-navigator">
-        <div className="bottom-navigator-child">
-          <div
-            className={`bottom-navigator-side-item ${location.pathname.includes("dashboard") ? "bottom-navigator-selected" : ""}`}
-          >
-            <Link to="/dashboard">
-              <House strokeWidth={1.5} color="black" />
-            </Link>
-          </div>
-          <div
-            className={`bottom-navigator-side-item ${location.pathname.includes("list") ? "bottom-navigator-selected" : ""}`}
-          >
-            <Link to="/list">
-              <AlignJustify color="black" strokeWidth={1.5} />
-            </Link>
-          </div>
-        </div>
-        <div className="bottom-navigator-middle">
-          <div className="bottom-navigator-circle">
-            <img
-              src={TestSvg}
-              alt="test"
-              onClick={openModal}
-              style={{ width: "32px" }}
+        <div
+          className={`bottom-navigator-child ${location.pathname.includes("dashboard") ? "bottom-navigator-selected" : ""}`}
+        >
+          <Link to="/dashboard">
+            <House
+              strokeWidth={1.5}
+              color="black"
+              className="buttom-navigator-icon"
             />
-          </div>
+          </Link>
         </div>
-        <div className="bottom-navigator-child">
-          <div
-            className={`bottom-navigator-side-item ${location.pathname.includes("trophys") ? "bottom-navigator-selected" : ""}`}
-          >
-            <Link to="/trophys">
-              <Trophy strokeWidth={1.5} color="black" />
-            </Link>
-          </div>
-          <div
-            className={`bottom-navigator-side-item ${location.pathname.includes("account") ? "bottom-navigator-selected" : ""}`}
-          >
-            <Link to="/account">
-              <CircleUserRound strokeWidth={1.5} color="black" />
-            </Link>
-          </div>
+
+        <div
+          className={`bottom-navigator-child ${location.pathname.includes("list") ? "bottom-navigator-selected" : ""}`}
+        >
+          <Link to="/list">
+            <AlignJustify
+              color="black"
+              strokeWidth={1.5}
+              className="buttom-navigator-icon"
+            />
+          </Link>
+        </div>
+
+        <div className={`bottom-navigator-child`}>
+          <CirclePlus
+            color="black"
+            strokeWidth={1.5}
+            onClick={openModal}
+            width={28}
+            height={28}
+          />
+        </div>
+
+        <div
+          className={`bottom-navigator-child ${location.pathname.includes("trophys") ? "bottom-navigator-selected" : ""}`}
+        >
+          <Link to="/trophys">
+            <Trophy
+              strokeWidth={1.5}
+              color="black"
+              className="buttom-navigator-icon"
+            />
+          </Link>
+        </div>
+
+        <div
+          className={`bottom-navigator-child ${location.pathname.includes("account") ? "bottom-navigator-selected" : ""}`}
+        >
+          <Link to="/account">
+            <CircleUserRound
+              strokeWidth={1.5}
+              color="black"
+              className="buttom-navigator-icon"
+            />
+          </Link>
         </div>
       </div>
+
       <MealModal open={openAddModal} onClose={closeModal} />
     </div>
   );
