5a0881a22a02815bce5a1a059a0c9e4038c53525
anixel <45008199+andifg@users.noreply.github.com>
2025-05-01 08:50:52 +0200

    add icon to average area

--- frontend/src/components/AverageMeatPortions/AverageMeatPortionsValues.tsx
+++ frontend/src/components/AverageMeatPortions/AverageMeatPortionsValues.tsx
@@ -1,11 +1,16 @@
+import { ServingCategory } from "@/client/types";
+import { ServingIcon } from "../ServingIcon/ServingIcon";
+
 type AverageMeatPortionsValuesProps = {
   currentValue?: number;
   lastValue?: number;
+  category: ServingCategory;
 };
 
 const AverageMeatPortionsValues = ({
   currentValue,
   lastValue,
+  category,
 }: AverageMeatPortionsValuesProps) => {
   let changeRate = 0;
   if (lastValue != undefined && lastValue != 0 && currentValue != undefined) {
@@ -14,13 +19,18 @@ const AverageMeatPortionsValues = ({
 
   return (
     <div className="average-meat-portions-value-header">
-      <div className="average-meat-portions-value">
-        {currentValue == undefined ? 0 : currentValue.toFixed(0)}
+      <div className="average-meat-portions-values-area">
+        <div className="average-meat-portions-value">
+          {currentValue == undefined ? 0 : currentValue.toFixed(0)}
+        </div>
+        <div
+          className={`average-meat-portions-diff ${changeRate < 0 && "average-meat-portions-diff-negative"} ${changeRate == 0 && "average-meat-portions-diff-zero"} ${changeRate > 0 && "average-meat-portions-diff-positive"}`}
+        >
+          {changeRate == undefined ? 0 : changeRate.toFixed(0)}%
+        </div>
       </div>
-      <div
-        className={`average-meat-portions-diff ${changeRate < 0 && "average-meat-portions-diff-negative"} ${changeRate == 0 && "average-meat-portions-diff-zero"} ${changeRate > 0 && "average-meat-portions-diff-positive"}`}
-      >
-        {changeRate == undefined ? 0 : changeRate.toFixed(0)}%
+      <div className="average-meat-portions-icon">
+        <ServingIcon servingCategory={category} />
       </div>
     </div>
   );
